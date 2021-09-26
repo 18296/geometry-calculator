21bc85638e42715b7622fd11897844cbf88a6dc3
182961 <87044961+18296@users.noreply.github.com>
2021-09-26 03:17:38 +0000

    User can add given variables

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import turtle
 import math
+library = {}
 
 def regular_polygons():
   while True:
-    no_sides = int(input("how many sides does your shape have?"))
-    for repeats in range(no_sides): # draws diagram
-      turtle.forward(100)
-      turtle.left((360/no_sides))
+    try:
+      no_sides = int(input("how many sides does your shape have?"))
+      if no_sides < 3:
+        int("a") # breaks code to except condition
+      for repeats in range(no_sides): 
+        turtle.forward(100)
+        turtle.left((360/no_sides))
+      turtle.forward(50)
+      turtle.write("side")
+      turtle.left(90)
+      angle = ((((no_sides-2)*180)/no_sides)/2)*(math.pi/180)
+      apothem = (100/2)*(math.tan(angle))
+      turtle.forward(apothem)
+      turtle.write("apothem") # draws diagram
+      break
+
+    except:
+      pass
+    
+  choice = input("write in given variables format: area:1, side:2")
+  data = choice.split(",")
+  for pair in data:
+    print("pair:", pair)
+    key = pair.split(":")[0]
+    value = pair.split(":")[1]
+    library[key] = value
+    print(library)
+
+  
 
 
 
