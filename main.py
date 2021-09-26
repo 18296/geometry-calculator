fabc7dc71f4cdc117e4e61f1c40e3e4ec44e7c4c
182961 <87044961+18296@users.noreply.github.com>
2021-09-26 11:00:34 +0000

    Basic structure. No calculations.

--- main.py
+++ main.py
@@ -2,22 +2,47 @@ import turtle
 import math
 library = {}
 
+
+def shape_drawing(shape, no_sides):
+  if shape == "polgon":
+    for repeats in range(no_sides): 
+      turtle.forward(100)
+      turtle.left((360/no_sides))
+
+    turtle.forward(50)
+    turtle.write("perimeter")
+    turtle.left(90)
+    angle = ((((no_sides-2)*180)/no_sides)/2)*(math.pi/180)
+    apothem = (100/2)*(math.tan(angle))
+    turtle.forward(apothem)
+    turtle.write("apothem") # draws diagram
+
+
+
+
+def polygon_maths(value, type):
+  if type == "area":
+    print(value)
+
+  if type == "apothem":
+    print(value)
+
+  if type == "perimeter":
+    print(value) # figure the math out later
+  
+  
+
+
+
+
 def regular_polygons():
   while True:
     try:
       no_sides = int(input("how many sides does your shape have?"))
       if no_sides < 3:
         int("a") # breaks code to except condition
-      for repeats in range(no_sides): 
-        turtle.forward(100)
-        turtle.left((360/no_sides))
-      turtle.forward(50)
-      turtle.write("side")
-      turtle.left(90)
-      angle = ((((no_sides-2)*180)/no_sides)/2)*(math.pi/180)
-      apothem = (100/2)*(math.tan(angle))
-      turtle.forward(apothem)
-      turtle.write("apothem") # draws diagram
+      shape_drawing("polgon",no_sides)
+      
       break
 
     except:
@@ -30,9 +55,15 @@ def regular_polygons():
     key = pair.split(":")[0]
     value = pair.split(":")[1]
     library[key] = value
-    print(library)
 
-  
+  if "area" in library:
+    polygon_maths(library.get("area"),"area") 
+
+  if "apothem" in library:
+    polygon_maths(library.get("apothem"),"apothem")
+
+  if "perimeter" in library:
+    polygon_maths(library.get("perimeter"),"perimeter")
 
 
 
