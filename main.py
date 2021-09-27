df2f0a31e462131d3d33d331c6cf1dd95e3f178d
182961 <87044961+18296@users.noreply.github.com>
2021-09-27 05:10:24 +0000

    Formulas for "r" input working

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import turtle
-import math
+import mpmath as math
+
 library = {}
 
 
-def shape_drawing(shape, no_sides):
+def shape_drawing(shape, n):
   if shape == "polgon":
-    for repeats in range(no_sides): 
+    for repeats in range(n): 
       turtle.forward(100)
-      turtle.left((360/no_sides))
+      turtle.left((360/n))
 
     turtle.forward(50)
-    turtle.write("perimeter")
+    turtle.write("   a")
     turtle.left(90)
-    angle = ((((no_sides-2)*180)/no_sides)/2)*(math.pi/180)
-    apothem = (100/2)*(math.tan(angle))
-    turtle.forward(apothem)
-    turtle.write("apothem") # draws diagram
+    angle = ((((n-2)*180)/n)/2)*(math.pi/180)
+    r = (100/2)*(math.tan(angle))
+    turtle.forward(r/2)
+    turtle.write("  r") # draws diagram
+    turtle.forward(r/2)
+    R = r*math.sec(math.pi/n)
+    turtle.forward(R/2)
+    turtle.write("  R")
+    turtle.forward(R/2)
+    turtle.left(90)
+    turtle.circle(R)
+    turtle.write("     C")
 
 
 
 
-def polygon_maths(value, type):
-  if type == "area":
-    print(value)
+def polygon_maths(value, type,n):
+  angle = ((((n-2)*180)/n)/2)*(math.pi/180)
 
-  if type == "apothem":
-    print(value)
 
-  if type == "perimeter":
+  if type == "r":
+
+    r = value
+
+    A = n*r*2*math.tan(math.pi/n)*10
+
+    
+    a = (value/(math.tan(angle)))*2
+    # a = (A/n)/10
+
+
+    P = a*n
+    
+
+    R = value*math.sec(math.pi/n)
+
+    C = 2*math.pi*R
+    
+    print(R,P,a,C,A)
+
+
+
+  if type == "a":
     print(value) # figure the math out later
   
   
@@ -38,39 +66,41 @@ def polygon_maths(value, type):
 def regular_polygons():
   while True:
     try:
-      no_sides = int(input("how many sides does your shape have?"))
-      if no_sides < 3:
+      n = int(input("how many as does your shape have?"))
+      if n < 3:
         int("a") # breaks code to except condition
-      shape_drawing("polgon",no_sides)
+      shape_drawing("polgon",n)
       
       break
 
     except:
       pass
     
-  choice = input("write in given variables format: area:1, side:2")
+  choice = input("write in given variables format: area:1, a:2")
   data = choice.split(",")
   for pair in data:
-    print("pair:", pair)
     key = pair.split(":")[0]
     value = pair.split(":")[1]
     library[key] = value
 
-  if "area" in library:
-    polygon_maths(library.get("area"),"area") 
+  if "r" in library:
+    r = int(library.get("r"))
+    polygon_maths(r,"r",n)
 
-  if "apothem" in library:
-    polygon_maths(library.get("apothem"),"apothem")
+  if "a" in library:
+    a = int(library.get("a"))
+    polygon_maths(a,"a",n)
 
-  if "perimeter" in library:
-    polygon_maths(library.get("perimeter"),"perimeter")
+  if "A" in library:
+    A = int(library.get("A"))
+    polygon_maths(A,"A",n)
 
 
 
 
 # start of code
 while True:
-    select = input("what area would you like to calculate?: Regular polygon (1) Circles (2) Triangles (3)")
+    select = input("what would you like to calculate?: Regular polygon (1) Circles (2) Triangles (3)")
     if select == "1":
         regular_polygons()
     if select == "2":
